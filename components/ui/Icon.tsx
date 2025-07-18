b05806370005f84df0e71c975d1759e9f952d589
Maybelpm <perezmaybel@gmail.com>
2025-07-18 00:51:52 -0400

    Update Icon.tsx

--- components/ui/Icon.tsx
+++ components/ui/Icon.tsx
@@ -17,7 +17,11 @@ const icons: { [key: string]: React.ReactNode } = {
     <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z" />
   ),
   reports: (
-    <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 6a7.5 7.5 0 100 15 7.5 7.5 0 000-15z" /><path strokeLinecap="round" strokeLinejoin="round" d="M10.5 10.5h.008v.008h-.008v-.008z" /> <path strokeLinecap="round" strokeLinejoin="round" d="M12 7.5h.008v.008H12V7.5z" />
+    <>
+      <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 6a7.5 7.5 0 100 15 7.5 7.5 0 000-15z" />
+      <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 10.5h.008v.008h-.008v-.008z" />
+      <path strokeLinecap="round" strokeLinejoin="round" d="M12 7.5h.008v.008H12V7.5z" />
+    </>
   ),
   plus: (
     <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
